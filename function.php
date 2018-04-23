1cd79648adef89fa95d72b35001d085f46bbc41e
MAKMAL 16 <makmal16@kpkt.gov.local>
2018-04-23 14:54:13 +0800

    function in function

--- function.php
+++ function.php
@@ -3,7 +3,8 @@
  * Fungsi operasi tambah
  */
 //Perlu menerima 3 parameter
-function tambah($param1, $param2, $param3 = 1){
+function tambah($param1, $param2, $param3 = 1)
+{
     return $param1 + $param2 + $param3;
 }
 
@@ -11,8 +12,36 @@ function tambah($param1, $param2, $param3 = 1){
  * Fungsi operasi darab
  */
 //Perlu menerima 3 parameter
-function darab($param1, $param2, $param3 = 1){
+function darab($param1, $param2, $param3 = 1)
+{
     return $param1 * $param2 * $param3;
+}
+
+/**
+ * Fungsi operasi tolak
+ */
+//Perlu menerima 3 parameter
+function tolak($param1, $param2, $param3 = 1)
+{
+    if (semak_int($param1) &&
+        semak_int($param2) &&
+        semak_int($param3)
+    ) {
+        return $param1 - $param2 - $param3;
+    } else {
+        return 'Sila semak nilai parameter';
+    }
+
+}
+
+/**
+ * Fungsi semak parameter
+ * @param $param
+ * @return bool
+ */
+function semak_int($param)
+{
+    return is_numeric($param) ? true : false;
 }
 
 
@@ -21,14 +50,19 @@ $b = 6;
 $c = 5;
 //Panggil fungsi bernama tambah
 //Dengan 3 parameter
-echo tambah($a,$c,$b);
+echo tambah($a, $c, $b);
 
 echo '<br/>';
 //Panggil fungsi bernama tambah
 //Dengan 3 parameter
-echo tambah($a,$b);
+echo tambah($a, $b);
 
 echo '<br/>';
 //Panggil fungsi bernama darab
 //Dengan 3 parameter
-echo darab($a,$b);
+echo darab($a, $b);
+
+echo '<br/>';
+//Panggil fungsi bernama tolak
+//Dengan 3 parameter
+echo "hasil tolak $a - $b - $c = ".tolak($a, $b, $c);
